89c2e9f5e40b89f6e1f2f22229192a8dfd941f9a
Bheart Shrestha <bheartshrestha7@gmail.com>
2020-07-29 18:00:14 +0545

    Updates

---
+++ precache-manifest.af42dd581271f13f9a4bcccf010c1577.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "678ad03c3bc80f977164f92b1774aab9",
+    "url": "/google-books/index.html"
+  },
+  {
+    "revision": "33dcfc320a1aaa7bacd2",
+    "url": "/google-books/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "a10544e82870d5cd9fec",
+    "url": "/google-books/static/css/main.02e99ab9.chunk.css"
+  },
+  {
+    "revision": "33dcfc320a1aaa7bacd2",
+    "url": "/google-books/static/js/2.24b6e781.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/google-books/static/js/2.24b6e781.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a10544e82870d5cd9fec",
+    "url": "/google-books/static/js/main.382f4364.chunk.js"
+  },
+  {
+    "revision": "52c6f95ad72c0d107b08",
+    "url": "/google-books/static/js/runtime-main.93b1d62a.js"
+  }
+]);
